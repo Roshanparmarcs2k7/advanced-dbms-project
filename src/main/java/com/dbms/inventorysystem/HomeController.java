f24aca42855f48230fa81faf078a8937215e5e13
Roshan <tumhari@email.com>
2026-05-29 13:56:22 +0500

    Dashboard improved with stats and better UI

--- src/main/java/com/dbms/inventorysystem/HomeController.java
+++ src/main/java/com/dbms/inventorysystem/HomeController.java
@@ -1,13 +1,48 @@
 package com.dbms.inventorysystem;
 
+import com.dbms.inventorysystem.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @GetMapping("/")
-    public String home() {
-        return "index"; 
+    public String home(Model model) {
+        model.addAttribute("totalProducts", productRepository.count());
+        model.addAttribute("totalCustomers", customerRepository.count());
+        model.addAttribute("totalSuppliers", supplierRepository.count());
+        model.addAttribute("totalOrders", orderRepository.count());
+        model.addAttribute("totalEmployees", employeeRepository.count());
+        model.addAttribute("totalWarehouses", warehouseRepository.count());
+        model.addAttribute("totalUsers", userRepository.count());
+        model.addAttribute("totalCategories", categoryRepository.count());
+        return "index";
     }
 }
